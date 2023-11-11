de853abc68415cfa35c080f60161fec7e06ded7d
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-11 08:16:41 +0530

    client/src/App.js- Routes added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
-import Layout from "./components/Layout/Layout";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
-      <Layout />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
