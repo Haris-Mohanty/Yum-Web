e61207201f68dc7054e3672292c277708badeab4
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-11 08:59:19 +0530

    client/src/styles/HeaderStyles.css.css- Navbar styling processing.....

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import logo from "../../Assets/logo.png";
+import "../../styles/HeaderStyles.css";
 
 const Header = () => {
   return (
@@ -9,10 +10,32 @@ const Header = () => {
       <Box>
         <AppBar component={"nav"} sx={{ bgcolor: "#B2BEB5" }}>
           <Toolbar>
+            {/* LOGO */}
             <Box width={"9%"} ml={2}>
               <Link to={"/"}>
                 <img src={logo} alt="Logo" width={"100%"} />
               </Link>
+            </Box>
+
+            {/* NAV MENU */}
+            <Box
+              ml={"auto"}
+              sx={{ display: { xs: "none", sm: "none", md: "block" } }}
+            >
+              <ul className="nav-menu">
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/menu"}>Menu</Link>
+                </li>
+                <li>
+                  <Link to={"/about"}>About</Link>
+                </li>
+                <li>
+                  <Link to={"/contact"}>Contact</Link>
+                </li>
+              </ul>
             </Box>
           </Toolbar>
         </AppBar>
