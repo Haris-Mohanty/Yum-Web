03958f2385093ef6aabe6a526981b7a2af3b51fc
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-12 09:56:37 +0530

    client/src/components/Layout/Footer.jsx- Footer section code fixed.

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstaIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
-export default Footer
+  return (
+    <>
+      <Box
+        sx={{
+          textAlign: "center",
+          bgcolor: "#B2BEB5",
+          color: "#006666",
+          padding: 2,
+        }}
+      >
+        <Typography variant={isSmallScreen ? "h7" : "h5"}>
+          All Rights Reserved &copy; Haris Mohanty!
+        </Typography>
+        <Box
+          sx={{
+            my: 2,
+            "& svg": {
+              fontSize: "35px",
+              cursor: "pointer",
+              mr: 2,
+            },
+            "& svg:hover": {
+              color: "#004c4c",
+              transform: "scale(1.3)",
+              transition: "all 400ms linear",
+            },
+          }}
+        >
+          <FacebookIcon />
+          <YouTubeIcon />
+          <InstaIcon />
+          <TwitterIcon />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Footer;
