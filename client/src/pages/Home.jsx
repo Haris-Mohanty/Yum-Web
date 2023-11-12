276d64ac930ef01bbb17bcde551555c7800c2bbd
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-12 10:15:22 +0530

    client/src/pages/Home.jsx- Homepage design processing....

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import Layout from "../components/Layout/Layout";
 import { Box, Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import Banner from "../Assets/banner.jpg";
 
 const Home = () => {
   return (
     <Layout>
-      <Box sx={{ mt: 10, mb: 5 }}>
+      <Box
+        sx={{
+          m: "4rem 0 0 0",
+          backgroundImage: `url(${Banner})`,
+          height: "90vh",
+          display:"flex",
+          justifyContent:'center',
+          alignItems:"center"
+        }}
+      >
         <Box>
           <Typography variant="h5">Food Website</Typography>
           <Typography variant="subtitle1">Best Food In India!</Typography>
