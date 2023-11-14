aba23aae30af2d4818cee8758a9a125856348248
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-14 06:53:48 +0530

    client/src/pages/Contact.jsx- Contact form creating code processing....

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
-import { Box, Typography } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 
 const Contact = () => {
   return (
     <Layout>
-      <Box sx={{ mt: 8 }}>
-        <Typography>Contact Us</Typography>
+      <Box sx={{ mt: 8, p: 4 }}>
+        <Box>
+          <Typography>Contact Us</Typography>
+        </Box>
+        <Box sx={{ width: "40%", m: "auto" }}>
+          <form>
+            <TextField fullWidth margin="normal" label="Name" required />
+            <TextField fullWidth margin="normal" label="Password" required />
+            <TextField
+              fullWidth
+              multiline
+              rows={4}
+              margin="normal"
+              label="Write your message"
+              required
+            />
+          </form>
+        </Box>
       </Box>
     </Layout>
   );
