4efc1df81035f4228658f66916e6f36b78523047
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-14 07:14:38 +0530

    client/src/pages/Contact.jsx- Form submit code processing......

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout/Layout";
 import { Box, Button, TextField, Typography } from "@mui/material";
 
 const Contact = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
   return (
     <Layout>
-      <Box sx={{ mt: 8, p: 4 }}>
-        <Box sx={{ m: "auto" }}>
-          <Typography variant="h4" component="h1">
+      <Box sx={{ mt: 8, p: 4, mb: 6 }}>
+        <Box>
+          <Typography
+            variant="h4"
+            component="h1"
+            fontWeight={"bold"}
+            fontFamily={"verdana"}
+          >
             Contact Us
           </Typography>
         </Box>
-        <Box sx={{ width: "40%", m: "auto" }}>
+        <Box sx={{ width: "40%", m: "auto", p: 3 }}>
           <form>
-            <TextField fullWidth margin="normal" label="Name" required />
-            <TextField fullWidth margin="normal" label="Password" required />
+            <Typography variant="h5" sx={{ ml: 19, color: "#006666" }}>
+              Write a message to us!
+            </Typography>
             <TextField
+              name="name"
+              value={inputs.name}
+              fullWidth
+              margin="normal"
+              label="Enter Your Name"
+              required
+            />
+            <TextField
+              name="email"
+              value={inputs.email}
+              fullWidth
+              margin="normal"
+              label="Enter Your Email"
+              required
+            />
+            <TextField
+              name="message"
+              value={inputs.message}
               fullWidth
               multiline
               rows={4}
@@ -26,7 +56,13 @@ const Contact = () => {
             <Button
               type="submit"
               variant="contained"
-              sx={{ bgcolor: "#006666" }}
+              sx={{
+                bgcolor: "#006666",
+                width: "20%",
+                display: "flex",
+                float: "right",
+                mt: 2,
+              }}
             >
               Send
             </Button>
