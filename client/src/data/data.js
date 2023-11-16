b4291958c92b7dfe6a88a0fc8e2f1765998b5a6f
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-16 11:02:00 +0530

    client/src/data/data.js- Create data file for add all menu items code fixed.

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,6 +1,11 @@
 import Biriyani from "../Assets/biriyani.jpg";
 import Burger from "../Assets/burger.jpg";
 import ChikenTikaMasala from "../Assets/Chicken Tikka Masala.jpg";
+import Dosa from "../Assets/dosa.jpg";
+import FishCurry from "../Assets/fish curry.jpg";
+import Pizza from "../Assets/pizza.jpg";
+import Samosa from "../Assets/samosa.jpg";
+import Soup from "../Assets/Soup Bowl.jpg";
 
 export const MenuList = [
   {
@@ -20,5 +25,35 @@ export const MenuList = [
     desc: "Experience the exquisite taste of Chicken Tikka Masala, a culinary masterpiece featuring tender chicken in a luscious.",
     image: ChikenTikaMasala,
     price: 201,
+  },
+  {
+    name: "Dosa",
+    desc: "Delight in crispy perfection with our Dosa—thin, savory crepes filled with deliciously spiced fillings.",
+    image: Dosa,
+    price: 79,
+  },
+  {
+    name: "Fish Curry",
+    desc: "Savor the ocean's bounty in our Fish Curry—succulent fish simmered in a flavorful blend of spices.",
+    image: FishCurry,
+    price: 65,
+  },
+  {
+    name: "Pizza",
+    desc: "Indulge in our pizza perfection—crafted with premium ingredients for a symphony of flavors in every slice.",
+    image: Pizza,
+    price: 255,
+  },
+  {
+    name: "Samosa",
+    desc: "Crunch into our Samosas—golden parcels filled with spiced potatoes and peas, a crispy delight for your taste buds.",
+    image: Samosa,
+    price: 40,
+  },
+  {
+    name: "Soup",
+    desc: "Sip into comfort with our soul-soothing soup, a warm blend of fresh ingredients for a flavorful experience.",
+    image: Soup,
+    price: 145,
   },
 ];
