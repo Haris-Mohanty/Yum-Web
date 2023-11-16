b6d9101b39663f3801491f8825e8d12d8c6f4e67
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-16 10:57:02 +0530

    client/src/data/data.js- Adding food items, desc, images and priceses....

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,10 +1,24 @@
-import biriyani from "../Assets/biriyani.jpg";
+import Biriyani from "../Assets/biriyani.jpg";
+import Burger from "../Assets/burger.jpg";
+import ChikenTikaMasala from "../Assets/Chicken Tikka Masala.jpg";
 
 export const MenuList = [
   {
-    name: "",
-    desc: "",
-    image: "hs",
-    price: 200,
+    name: "Biriyani",
+    desc: "Savor the rich flavors of our Biryani, a symphony of fragrant rice, succulent meat, and aromatic spices in every bite.",
+    image: Biriyani,
+    price: 199,
+  },
+  {
+    name: "Burger",
+    desc: "Indulge in our mouthwatering burgers - a perfect blend of juicy patties, fresh veggies, and irresistible sauces for a delightful bite.",
+    image: Burger,
+    price: 155,
+  },
+  {
+    name: "Chiken Tika Masala",
+    desc: "Experience the exquisite taste of Chicken Tikka Masala, a culinary masterpiece featuring tender chicken in a luscious.",
+    image: ChikenTikaMasala,
+    price: 201,
   },
 ];
