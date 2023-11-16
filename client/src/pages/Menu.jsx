701ac205542d27638a96e3ff8ba61f3fd4d1ac24
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-16 11:04:04 +0530

    client/src/pages/Menu.jsx- Adding menu items in ui code processing.....

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import {MenuList} from '../data/data'
+import { Box } from '@mui/material'
 
 const Menu = () => {
   return (
     <Layout>
-      <h1>Menu Page</h1>
+      <Box>
+        {}
+      </Box>
     </Layout>
   )
 }
