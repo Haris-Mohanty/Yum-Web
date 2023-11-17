b314e4c51fb62044ddf3234f686dd2b85cc3a8a3
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-17 09:51:18 +0530

    client/src/components/Layout/Header.jsx- Active navlink code fixed.

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -64,16 +64,16 @@ const Header = () => {
             >
               <ul className="nav-menu">
                 <li>
-                  <Link to={"/"}>Home</Link>
+                  <NavLink to={"/"}>Home</NavLink>
                 </li>
                 <li>
-                  <Link to={"/menu"}>Menu</Link>
+                  <NavLink to={"/menu"}>Menu</NavLink>
                 </li>
                 <li>
-                  <Link to={"/about"}>About</Link>
+                  <NavLink to={"/about"}>About</NavLink>
                 </li>
                 <li>
-                  <Link to={"/contact"}>Contact</Link>
+                  <NavLink to={"/contact"}>Contact</NavLink>
                 </li>
               </ul>
             </Box>
