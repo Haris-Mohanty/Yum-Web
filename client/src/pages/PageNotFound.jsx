12e9f0e2bf7da16fed60615c33178ca52f58dec2
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-17 10:07:31 +0530

    client/src/pages/PageNotFound.jsx- Page not found when a user use wrong routes code fixed.

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,15 +1,37 @@
 import React from "react";
-import Layout from "../components/Layout/Layout";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
 
 const PageNotFound = () => {
   return (
-    <Layout>
-      <Box textAlign="center" mt={4}>
-      <Typography variant="h4">404 - Not Found</Typography>
-      <Typography variant="body1">The page you are looking for does not exist.</Typography>
-    </Box>
-
-    </Layout>
+    <>
+      <Box textAlign="center" mt={6} p={4}>
+        <Grid container justifyContent="center" alignItems="center" spacing={2}>
+          <Grid item>
+            <SentimentDissatisfiedIcon color="error" style={{ fontSize: 60 }} />
+          </Grid>
+          <Grid item>
+            <Typography variant="h4" color="error" fontWeight={'bold'}>
+              404 - Not Found
+            </Typography>
+            <Typography variant="body1" mt={2}>
+              Oops! The page you are looking for does not exist.
+            </Typography>
+            <Box mt={4}>
+              <Button
+                component={Link}
+                to="/"
+                variant="contained"
+                color="primary"
+              >
+                Go to Home
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
 };
 
