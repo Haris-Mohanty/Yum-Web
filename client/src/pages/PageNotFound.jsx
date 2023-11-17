19adf9102175c9a11248d54ea41ee85ab6bc2278
Haris Mohanty <harismohanty8658@gmail.com>
2023-11-17 09:58:35 +0530

    client/src/pages/PageNotFound.jsx- Page not found when a user use wrong routes code processing.....

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -4,7 +4,11 @@ import Layout from "../components/Layout/Layout";
 const PageNotFound = () => {
   return (
     <Layout>
-      <h1>Page not found!</h1>
+      <Box textAlign="center" mt={4}>
+      <Typography variant="h4">404 - Not Found</Typography>
+      <Typography variant="body1">The page you are looking for does not exist.</Typography>
+    </Box>
+
     </Layout>
   );
 };
